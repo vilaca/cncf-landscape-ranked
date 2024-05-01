b047e75694f7bd8662376c6866b0acc684cdf19f
João Vilaça <vilaca@users.noreply.github.com>
2024-05-01 15:39:26 +0100

    remove dupe repos

--- create-repo-list.sh
+++ create-repo-list.sh
@@ -1,4 +1,5 @@
 yq '.landscape.[].subcategories.[].items.[].repo_url' < $1 | grep -Fxv null \
+    | uniq \
     | cut -d '/' -f4-5 \
     | while read repo; do curl -s -L "https://api.github.com/repos/$repo"  --header "Authorization: Bearer $PAT" \
     | jq -r '"\(.stargazers_count)|\(.full_name)|\(.license.name)|\(.description)"'; done \
