67ac8428341299487de1ad593597a832ee7f6e68
vilaca <jvilaca@gmail.com>
2024-05-01 12:21:08 +0100

    extract and calc

---
+++ create-repo-list.sh
@@ -0,0 +1,5 @@
+yq '.landscape.[].subcategories.[].items.[].repo_url' < $1 | grep -Fxv null \
+    | cut -d '/' -f4-5 \
+    | while read repo; do curl -s -L "https://api.github.com/repos/$repo"  --header "Authorization: Bearer $PAT" \
+    | jq -r '"\(.stargazers_count)|\(.full_name)|\(.license.name)|\(.description)"'; done \
+    > $2
