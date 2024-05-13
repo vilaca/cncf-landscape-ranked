84b724af2567cccbf32bd989f65487907852026c
vilaca <jvilaca@gmail.com>
2024-05-14 00:43:39 +0100

    fix

--- utils/sevenDaysAgo.sh
+++ utils/sevenDaysAgo.sh
@@ -1,5 +1,5 @@
 current_date=$(date +%s)
 seven_days=$((7 * 24 * 60 * 60))
 seven_days_ago=$((current_date - seven_days))
-seven_days_ago_date=$(date -d "@$seven_days_ago" +%d-%m-%Y)
+seven_days_ago_date=$(date -d "@$seven_days_ago" +%Y-%m-%d)
 echo $seven_days_ago_date
